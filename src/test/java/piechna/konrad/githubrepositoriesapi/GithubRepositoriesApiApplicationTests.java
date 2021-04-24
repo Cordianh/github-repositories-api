3d7b767d3903c855342081094a69ac77ddd21781
Konrad Piechna <piechnakonrad@gmail.com>
2021-04-24 14:48:26 +0200

    Added deploy configuration.

--- src/test/java/piechna/konrad/githubrepositoriesapi/GithubRepositoriesApiApplicationTests.java
+++ src/test/java/piechna/konrad/githubrepositoriesapi/GithubRepositoriesApiApplicationTests.java
@@ -2,8 +2,9 @@ package piechna.konrad.githubrepositoriesapi;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import piechna.konrad.githubrepositoriesapi.repository.GithubRepositoriesApiApplication;
 
-@SpringBootTest
+@SpringBootTest(classes = GithubRepositoriesApiApplication.class)
 class GithubRepositoriesApiApplicationTests {
 
     @Test
